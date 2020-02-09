100bd3a20cd29e403aca4a16eaa0cfa6f7dc327c
Gabriel <kingnachos3456@yahoo.com>
2020-02-09 02:42:39 -0500

    finish redbud unit test with mockked data

---
+++ app/src/test/java/edu/uc/jonesbr/myplantdiary/PlantDataUnitTest.kt
@@ -0,0 +1,97 @@
+package edu.uc.jonesbr.myplantdiary
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import edu.uc.jonesbr.myplantdiary.dto.Plant
+import edu.uc.jonesbr.myplantdiary.service.PlantService
+import edu.uc.jonesbr.myplantdiary.ui.main.MainViewModel
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.rules.TestRule
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class PlantDataUnitTest {
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+    lateinit var mvm: MainViewModel
+
+    var plantService = mockk<PlantService>()
+
+    @Test
+    fun confirmEasterRedBudOuputsEasternRedBud () {
+        var plant: Plant = Plant("Cercis", "canadesis", "Eastern Redbud")
+        assertEquals("Eastern Redbud",plant.toString());
+    }
+
+    @Test
+    fun searchForRedbud_returnsRedbud() {
+        givenAFeedOfMockedPlantDataAreAvailable()
+        whenSearchForRedbud()
+        thenResultContainsEasternsRedbud()
+    }
+
+    private fun givenAFeedOfMockedPlantDataAreAvailable() {
+        mvm = MainViewModel()
+        createMockData()
+    }
+
+    private fun createMockData() {
+        var allPlantsLiveData = MutableLiveData<ArrayList<Plant>>()
+        var allPlants = ArrayList<Plant>()
+        //create and add plants to our collection
+        var redbud = Plant("Cercis", "canadesis", "Eastern Redbud")
+        allPlants.add(redbud)
+        var redOak = Plant("Cercis", "canadesis", "Eastern Redbud")
+        allPlants.add(redOak)
+        var whiteOak = Plant("Cercis", "canadesis", "Eastern Redbud")
+        allPlants.add(whiteOak)
+        allPlantsLiveData.postValue(allPlants)
+        every {plantService.fetchPlants(any<String>())} returns allPlantsLiveData
+        mvm.plantService = plantService
+    }
+
+    private fun whenSearchForRedbud() {
+        mvm.fetchPlants("redbud")
+    }
+
+    private fun thenResultContainsEasternsRedbud() {
+        var redbudFound = false;
+        mvm.plants.observeForever {
+            //here is where we do the observing
+            assertNotNull(it)
+            assertTrue(it.size > 0)
+            it.forEach{
+                if(it.genus == "Cercis" && it.species == "canadensis" && it.common.contains("Eastern Redbud")) {
+                    redbudFound = true
+                }
+            }
+        }
+        assertTrue(redbudFound)
+
+        fun whenISearchForGarbage() {
+            mvm.fetchPlants("sklujapouetllkjsdau")
+        }
+
+        fun thenIGetZeroResults() {
+            mvm.plants.observeForever {
+                assertEquals(0, it.size)
+            }
+        }
+
+        @Test
+        fun searchForGarbage_returnsNothing() {
+            givenAFeedOfMockedPlantDataAreAvailable()
+            whenISearchForGarbage()
+            thenIGetZeroResults()
+        }
+    }
+}
