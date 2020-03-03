f424ea795993c53ccaed43eb28ffc25d3e71ffad
Gabriel <kingnachos3456@yahoo.com>
2020-03-02 23:26:18 -0500

    use firebase to save plant data in the cloud

--- app/src/main/java/edu/uc/jonesbr/myplantdiary/ui/main/MainViewModel.kt
+++ app/src/main/java/edu/uc/jonesbr/myplantdiary/ui/main/MainViewModel.kt
@@ -1,21 +1,40 @@
 package edu.uc.jonesbr.myplantdiary.ui.main
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import edu.uc.jonesbr.myplantdiary.dto.Plant
+import edu.uc.jonesbr.myplantdiary.dto.Specimen
 import edu.uc.jonesbr.myplantdiary.service.PlantService
 
 class MainViewModel : ViewModel() {
     private var _plants: MutableLiveData<ArrayList<Plant>> = MutableLiveData<ArrayList<Plant>>()
     var plantService: PlantService = PlantService()
+    private lateinit var firestore : FirebaseFirestore
 
     init {
        fetchPlants("e")
+        firestore = FirebaseFirestore.getInstance()
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
     }
 
 
     fun fetchPlants(plantName: String) {
         _plants = plantService.fetchPlants(plantName)
+    }
+
+    fun save(specimen: Specimen) {
+        val document = firestore.collection("specimens").document()
+        specimen.specimenId = document.id
+        val set = document.set(specimen)
+            set.addOnSuccessListener {
+                Log.d("Firebase", "document saved")
+            }
+            set.addOnFailureListener {
+                Log.d("Firebase", "document saved")
+            }
     }
 
     internal var plants: MutableLiveData<ArrayList<Plant>>
